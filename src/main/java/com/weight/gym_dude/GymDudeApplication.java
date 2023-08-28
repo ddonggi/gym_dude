fa4ecff80bde1a5d30498b3921f9ea276764d2f1
dglee <ldg6153@gmail.com>
2023-08-28 16:41:40 +0900

    Initial commit

---
+++ src/main/java/com/weight/gym_dude/GymDudeApplication.java
@@ -0,0 +1,13 @@
+package com.weight.gym_dude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymDudeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymDudeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/weight/gym_dude/GymDudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weight.gym_dude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymDudeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

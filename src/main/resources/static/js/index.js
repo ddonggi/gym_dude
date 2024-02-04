691e197ebcc18f09f170e77adfc4e3d63f1b1774
dglee <ldg6153@gmail.com>
2024-02-05 01:33:45 +0900

    UPDATE | spring security config

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,4 +1,17 @@
-console.log('index.js')
+window.onload = () => {
+    console.log('index.js')
+
+    // console.log('page test');
+// let size = [[${paging.size}]];
+    console.log('size of page:', size);
+    console.log('total pages:', totalPages);
+    console.log('total elements:', totalElements);
+    console.log('page number:', number);
+    console.log('page has pre:', hasPrevious);
+    console.log('page has next:', hasNext);
+// let paging = "[[${paging}]]";
+//     console.log('paging:', paging);
+}
 /*
 document.querySelector(".signup").addEventListener('click',()=>{
     location.href="/signup";

4600969b6a82e14c70c8dcdb1bc56b46038e2da1
dglee <ldg6153@gmail.com>
2024-02-07 02:03:34 +0900

    UPDATE | feed manage menu in html,css,js

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,5 +1,10 @@
 window.onload = () => {
     console.log('index.js')
+    let deleteQuestion = async(questionId) =>{
+        const response = await fetch("/delete/"+questionId);
+        const jsonData = await response.json();
+        console.log('jsondata:',jsonData);
+    }
 
     // console.log('page test');
 // let size = [[${paging.size}]];
@@ -11,8 +16,23 @@ window.onload = () => {
     console.log('page has next:', hasNext);
 // let paging = "[[${paging}]]";
 //     console.log('paging:', paging);
-    document.querySelector(".option-button").addEventListener('click',()=>{
-        console.log('option-button click')
+    document.querySelectorAll(".option-button").forEach(optionButton=> {
+        optionButton.addEventListener('click', () => {
+            optionButton.classList.toggle("rotate90");
+            optionButton.previousElementSibling.classList.toggle("option-toggle");
+            optionButton.parentElement.querySelector(".delete-button").addEventListener('click',()=>{
+                if(confirm("정말 삭제 하시겠습니까? 삭제 후엔 되돌릴 수 없습니다")) {
+                    console.log("예 눌렀슴더");
+                    let id=optionButton.parentElement.id;
+                    console.log(optionButton.parentElement.id);
+                    deleteQuestion(id).then(data=>console.log('data:',data));
+                    // alert("삭제 완료");
+                }
+                // }else{
+                    // alert("제출실패");
+                // }
+            })
+        })
     })
 
     document.querySelectorAll(".feed-body").forEach((feedBody)=>{

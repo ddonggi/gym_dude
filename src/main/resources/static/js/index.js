b654f6726cf0923eb9538ec8e5728c69a7d1046b
dglee <ldg6153@gmail.com>
2024-02-15 22:29:19 +0900

    UPDATE | page scroll render(content, media)

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -10,30 +10,15 @@ import {
     setFeedContentHeight,
     setCommentToggleEvent,
     setLikeToggleEvent,
+    setFeedEvent,
     io,
     observeLastItem
 } from './utils/commonUtils.js';
 
 //로그인된 유저의 헤더 프로필 이미지에 토글 이벤트 등록
 setHeaderProfileEvent();
-
-// 피드(게시물)에 이벤트 등록
-if (document.querySelectorAll(".feed")) {
-    document.querySelectorAll(".feed").forEach((feed) => {
-        /* 본인 게시물 : 옵션 토글, 게시물 수정,삭제 이벤트 등록 */
-        if (feed.querySelector(".option-button")) {
-            setOptionToggleEvent(feed);
-            setModifyFormEvent(feed);
-            setModifyCancelEvent(feed);
-            setModifySaveEvent(feed);
-            setDeleteEvent(feed);
-        }
-        setFollowEvent(feed);
-        setCarouselEvent(feed);
-        setLikeToggleEvent(feed);
-        setCommentToggleEvent(feed);
-    })
-}
+setFeedEvent();
+setFeedContentHeight();
 /*옵저빙 등록*/
 observeLastItem(io, document.querySelectorAll('.feed'));
 

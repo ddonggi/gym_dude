6af9cb962a265eabe11ced5b8779dc6175f5543a
dglee <ldg6153@gmail.com>
2024-03-16 21:45:26 +0900

    ADD | 1RM calc controller

--- src/main/java/com/weight/gym_dude/etc/MainController.java
+++ src/main/java/com/weight/gym_dude/etc/MainController.java
@@ -1,14 +1,24 @@
 package com.weight.gym_dude.etc;
-/*
- * Created by 이동기 on 2023-09-12
- */
 
+import com.weight.gym_dude.user.SiteUser;
+import com.weight.gym_dude.user.SiteUserDTO;
+import com.weight.gym_dude.user.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+import java.util.List;
+
+@Slf4j
 @Controller
+@RequiredArgsConstructor //final이 붙은 속성을 포함하는 생성자를 자동으로 생성하는 역할을 한다
 public class MainController {
+    private final UserService userService;
 
 //    @RequestMapping("/")
 //    public String root() {
@@ -25,5 +35,30 @@ public class MainController {
     public String policy(){
 
         return "policy";
+    }
+    @GetMapping("/calc")
+    public String calc(
+            Model model,
+            Principal principal,
+            Authentication authentication
+    ){
+        log.info("----------------------"); // getName은 이메일임
+        if(principal!=null) {
+            String email = principal.getName();
+            if(authentication!=null){
+                if(authentication.getPrincipal() instanceof OAuth2User){
+                    log.info("change oauth2user!!!!!");
+                    OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+                    email =  oAuth2User.getAttributes().get("email").toString();
+                }
+            }
+            SiteUser principalUser = userService.getUser(email);//현재 로그인한 사용자의 이메일으로 db조회
+            SiteUserDTO siteUserDTO = userService.toUserDTO(principalUser);
+            log.info("feed page logined siteUser name:{}", siteUserDTO.getUserName());
+            model.addAttribute("siteUser", siteUserDTO);
+        }else{
+            log.info("Guest User");
+        }
+        return "calc";
     }
 }
